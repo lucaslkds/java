44cad5cee1c8361ccc290914ea8b7425f19b2d07
lucaslkds <91992988+lucaslkds@users.noreply.github.com>
2026-03-13 16:24:16 -0300

    Add files via upload

---
+++ Senha.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Senha {
+    
+    public static void main(String[] args) {
+        
+        Scanner tc = new Scanner(System.in)
+        String login;
+        String senha;
+        String verificar_senha;
+
+        System.out.print("Digite seu login:");
+        login = teclado.nextLine();
+        System.out.print("Digite sua nova senha:");
+        senha = teclado.nextLine();
+        
+
+        System.out.print("Qual a sua senha?:");
+        verificar_senha = teclado.nextLine()
+
+        
+        
+        
+
+
+
+    }
+
+}
